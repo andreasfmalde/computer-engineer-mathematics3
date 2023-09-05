22078e2dd0057272a5c224a7065f506e5d9da3b0
Andreas Follevaag Malde <andrefma@stud.ntnu.no>
2023-09-05 23:48:46 +0200

    Exercise 15,week2: Ax=B, LU factorial implementation

--- Uke2/Ex15.py
+++ Uke2/Ex15.py
@@ -5,16 +5,16 @@ def LUfactorize(A):
    L = np.eye(n)
    U = np.zeros((n,n))
    U = A.copy()
-   for j in ?????:
-      for i in range(?????):
-         if ( ???? ): #0 i pivot element
+   for j in range(n-1):
+      for i in range(j+1,n):
+         if ( U[j,j] == 0 ): #0 i pivot element
             raise np.linalg.LinAlgError("Zero pivot encountered")
             return 
-         mult = ?????
+         mult = U[i,j]/U[j,j]
          U[i,j]=0.0
-         L[i,j] = ????
-         for k in range(???, ???):
-            U[?,?] = ?????
+         L[i,j] = mult
+         for k in range(j+1,n):
+            U[i,k] = U[i,k] - (U[j,k]*mult)
    return L,U
 
    
@@ -22,24 +22,26 @@ def LUsolve(L,U,b):
 
    c = np.zeros_like(b)
    n = len(c) 
-   for i in ????:
+   for i in range(n):
       c[i] =b[i]
-      for j in ????
-         c[i] = c[i] - ??????
+      for j in range(i):
+         c[i] = c[i] - L[i,j]*c[j]
    
    x = c.copy()
-   for i in ?????:
-      for j in ??????:
-         x[i] = ?????
-      x[i] = ?????
+   for i in range(n-1,-1,-1):
+      for j in range(i+1,n):
+         x[i] = x[i] - U[i,j]*x[j]
+      x[i] = x[i]/U[i,i]
    
    return x
 
-A = ????
-b = ???
+A = np.array([3.0,1.0,2.0,6.0,3.0,4.0,3.0,1.0,5.0])
+A = A.reshape((3,3))
+b = np.array([0.0,1.0,3.0]).T
 try:
    L,U = LUfactorize(A)
    x = LUsolve(L,U,b)
+   print(x) # Fjern ved innlevering
 except np.linalg.LinAlgError as e:
    print(f"LinAlgError: {e}")
 
